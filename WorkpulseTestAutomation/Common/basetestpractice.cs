e5191a3bd8f3cf96ad5e49ccad0b6f147306429c
Hemant Agrawal <hemant.agrawal@workpulse.com>
2024-02-19 13:03:09 +0530

    commit

---
+++ WorkpulseTestAutomation/Common/basetestpractice.cs
@@ -0,0 +1,35 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using NUnit.Framework;
+using RazorEngine.Compilation.ImpromptuInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkpulseTestAutomation.Common
+{
+    public class basetestpractice
+    {
+
+        public static ExtentTest extent;
+        public static ExtentReports extentreport;
+        public static ExtentV3HtmlReporter reporter;
+       
+        [OneTimeSetUp]
+        public static void report()
+        {
+            var combinepath = Path.GetFullPath(Path.Combine(@"E:\Workpulse documents\Automation\WorkpulseTestAutomation\Reports")) + "ExtentReport" + DateTime.Now.ToString("_MMddyyyy_hhmmtt") + ".html";
+            extentreport = new ExtentReports();
+            reporter = new ExtentV3HtmlReporter(combinepath);
+            extentreport.AttachReporter(reporter);
+        }
+
+        [OneTimeTearDown]
+        public void extentflush()
+        {
+            extentreport.Flush();
+        }
+    }
+}
